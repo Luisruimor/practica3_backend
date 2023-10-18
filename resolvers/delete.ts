d75cd2c525e21eb9cb990d14c28eab83268bef96
Luis <luisruimore@gmail.com>
2023-10-18 18:05:09 +0200

    Endpoint delete
    Completado el método delete, para eliminar un dato buscándolo por su id

--- resolvers/delete.ts
+++ resolvers/delete.ts
@@ -1,3 +1,6 @@
-export const deleteDisco = () => {
-    //code
+import {discosCollection} from "../db/mongo.ts";
+
+export const deleteDisco = async (req:Request, res:Response) => {
+    const deleteDisco = await discosCollection.deleteOne({ id: req.params.id } );
+    deleteDisco>0? res.send("Se ha borrado correctamente"): res.send("No se ha encontrado ningún valor con ese id")
 }
