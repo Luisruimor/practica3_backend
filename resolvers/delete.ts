7d38277681b12c52e10015a1fd6c19ece1a69a0f
Luis <luisruimore@gmail.com>
2023-10-20 11:50:08 +0200

    Método delete arreglado
    Método delete arreglado, no estaba añadido ni la query de $eq (equal)

--- resolvers/delete.ts
+++ resolvers/delete.ts
@@ -1,6 +1,7 @@
 import {discosCollection} from "../db/mongo.ts";
+import {ObjectId} from "https://deno.land/x/web_bson@v0.3.0/mod.js";
 
 export const deleteDisco = async (req:Request, res:Response) => {
-    const deleteDisco = await discosCollection.deleteOne({ _id: req.params._id } );
+    const deleteDisco = await discosCollection.deleteOne({ _id: {$eq: new ObjectId(req.params.id) } } );
     deleteDisco>0? res.send("Se ha borrado correctamente"): res.send("No se ha encontrado ningún valor con ese id")
 }
