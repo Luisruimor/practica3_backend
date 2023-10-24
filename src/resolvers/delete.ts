bc7ec64814f787ea708b89ad2f3cf4cc77843772
Luis <luisruimore@gmail.com>
2023-10-24 16:03:06 +0200

    Arreglo delete

--- src/resolvers/delete.ts
+++ src/resolvers/delete.ts
@@ -2,6 +2,6 @@ import {discosCollection} from "../db/mongo.ts";
 import {ObjectId} from "https://deno.land/x/web_bson@v0.3.0/mod.js";
 
 export const deleteDisco = async (req:Request, res:Response) => {
-    const deleteDisco = await discosCollection.deleteOne({ _id: {$eq: new ObjectId(req.params.id) } } );
+    const deleteDisco = await discosCollection.deleteOne({ _id: {$eq: new ObjectId(req.params._id) } } );
     deleteDisco>0? res.send("Se ha borrado correctamente"): res.send("No se ha encontrado ningún valor con ese id")
 }
